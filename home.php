2d50cf7a673f91c231a4463056c57bd908f82b28
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-18 05:27:46 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,10 @@
+<?php include 'inc/header-carousel.php'; ?>
+
+<?=$this->layout('index');?>
+
+   <!-- section service -->
+   <?php include 'inc/components-3-grid-service.php'; ?>
+   
+   <?php include 'inc/components-berita.php'; ?>
+
+   <?php include 'inc/components-si-terpadu.php'; ?>
