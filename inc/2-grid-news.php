a35d4cd95797a6d395ac42f0ed94f8d38506cf52
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-18 05:26:20 +0800

    Create 2-grid-news.php

---
+++ inc/2-grid-news.php
@@ -0,0 +1,35 @@
+<div class="carousel slide carousel-fade" id="my-carousel" data-ride="carousel">
+           
+
+              <div class="carousel-inner">
+                <div class="carousel-item text-center carousel1 active">
+                  
+                    <img src="img/1.jpg"  class="img-responsive w-100">
+                  
+                  <h5>Gambar 1</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel2">
+                  
+                    <img src="img/1.jpg"  class="img-responsive w-100">
+                  
+                  <h5>Gambar 2</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel3">
+                  
+                    <img src="img/1.jpg"  class="img-responsive w-100">
+                  
+                  <h5>Gambar 3</h5>
+                  
+                </div>
+                <div class="carousel-item text-center carousel4">
+                  
+                    <img src="img/1.jpg"  class="img-responsive w-100">
+                  
+                  <h5>Gambar 4</h5>
+                  
+                </div>
+              </div>
+              
+        </div>
