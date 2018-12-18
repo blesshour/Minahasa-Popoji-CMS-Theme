f0429afffa7d37efe21ad1f8be68f2e8d43dae49
pricelangela <45728920+pricelangela@users.noreply.github.com>
2018-12-19 04:02:42 +0800

    update col-sm-12

--- inc/components-si-terpadu.php
+++ inc/components-si-terpadu.php
@@ -6,67 +6,67 @@
 
         <div class="row">
 
-        <div class="col-lg-3 col-md-6 col-sm-12 text-center">
-            <div class="box fadeInRight">
-                <div class="row in-box">
-                    <div class="pull-left icon col-3 text-center">
-                        <i class="fa fa-commenting"></i>
+            <div class="col-lg-3 col-sm-6 col-xs-12 text-center">
+                <div class="box fadeInRight">
+                    <div class="row in-box">
+                        <div class="pull-left icon col-3 text-center">
+                            <i class="fa fa-commenting"></i>
+                        </div>
+                        <div class="media-body caption col-9">
+                            <h3>Sistem Informasi</h5>
+                                <h2 class="media-heading">PANGAN</h4>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="media-body caption col-9">
-                        <h3>Sistem Informasi</h5>
-                        <h2 class="media-heading">PANGAN</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <div class="col-lg-3 col-md-6 col-sm-12 text-center">
-            <div class="box fadeInRight">
-                <div class="row in-box">
-                    <div class="pull-left icon col-3 text-center">
-                        <i class="fa fa-commenting"></i>
-                    </div>
-                    <div class="media-body caption col-9">
-                        <h3>Sistem Informasi</h5>
-                        <h2 class="media-heading">PANGAN</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    
+                    <div class="col-lg-3 col-sm-6 col-xs-12 text-center">
+                        <div class="box fadeInRight">
+                            <div class="row in-box">
+                                <div class="pull-left icon col-3 text-center">
+                                    <i class="fa fa-commenting"></i>
+                                </div>
+                                <div class="media-body caption col-9">
+                                    <h3>Sistem Informasi</h5>
+                                        <h2 class="media-heading">PANGAN</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-        <div class="col-lg-3 col-md-6 col-sm-12 text-center">
-            <div class="box fadeInRight">
-                <div class="row in-box">
-                    <div class="pull-left icon col-3 text-center">
-                        <i class="fa fa-commenting"></i>
-                    </div>
-                    <div class="media-body caption col-9">
-                        <h3>Sistem Informasi</h5>
-                        <h2 class="media-heading">PANGAN</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <div class="col-lg-3 col-md-6 col-sm-12 text-center">
-            <div class="box fadeInRight">
-                <div class="row in-box">
-                    <div class="pull-left icon col-3 text-center">
-                        <i class="fa fa-commenting"></i>
-                    </div>
-                    <div class="media-body caption col-9">
-                        <h3>Sistem Informasi</h5>
-                        <h2 class="media-heading">PANGAN</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
+                            <div class="col-lg-3 col-sm-6 col-xs-12 text-center">
+                                <div class="box fadeInRight">
+                                    <div class="row in-box">
+                                        <div class="pull-left icon col-3 text-center">
+                                            <i class="fa fa-commenting"></i>
+                                        </div>
+                                        <div class="media-body caption col-9">
+                                            <h3>Sistem Informasi</h5>
+                                                <h2 class="media-heading">PANGAN</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-3 col-sm-6 col-xs-12 text-center">
+                                        <div class="box fadeInRight">
+                                            <div class="row in-box">
+                                                <div class="pull-left icon col-3 text-center">
+                                                    <i class="fa fa-commenting"></i>
+                                                </div>
+                                                <div class="media-body caption col-9">
+                                                    <h3>Sistem Informasi</h5>
+                                                        <h2 class="media-heading">PANGAN</h4>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
 
 
-            
-        </div>
-        <!--/.row-->
-    </div>
-    <!--/.container-->
-</section>
+                                            
+                                        </div>
+                                        <!--/.row-->
+                                    </div>
+                                    <!--/.container-->
+                                </section>
 <!--/#services-->
