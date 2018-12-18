dd6f393bdfce766ab95ab5708963db5863f1113e
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-19 03:29:09 +0800

    Add files via upload

--- inc/components-socmed.php
+++ inc/components-socmed.php
@@ -1,26 +1,25 @@
 <section id="partner">
         <div class="container">
             <div class="center fadeInDown">
-                <h2>Our Partners</h2>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <h2>Social Media</h2>
             </div>
 
-            <div class="partners">
+            <div class="partners center">
                 <ul>
                     <li>
-                        <a href="#"><i class="fa fa-facebook fa-5x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+                        <a href="#"><i class="logo fa fa-facebook-square  fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-twitter-square fa-5x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+                        <a href="#"><i class="logo fa fa-twitter-square fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-instagram fa-5x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+                        <a href="#"><i class="logo fa fa-instagram fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-google-plus fa-5x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+                        <a href="#"><i class="logo fa fa-google-plus fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-youtube fa-5x fadeInDown" data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+                        <a href="#"><i class="logo fa fa-youtube fadeInDown" data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
                     </li>
                 </ul>
             </div>
