4bb660cea14a9eb103ddd4eed5f519bdf94452b5
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-19 04:56:55 +0800

    Update header.php

--- header.php
+++ header.php
@@ -5,26 +5,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-6 col-xs-12">
-                        <div class="top-number">
-                            <p><i class="fa fa-phone-square"></i> +0123 456 70 90</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-xs-12">
-                        <div class="social">
-                            <ul class="social-share">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                                <li><a href="#"><i class="fa fa-skype"></i></a></li>
-                            </ul>
-                            <div class="search">
-                                <form action="<?=BASE_URL;?>/search" method="post">
-									<input type="text" class="search-form" autocomplete="off" placeholder="<?=$this->e($front_search);?>...">
-									<i class="fa fa-search"></i>
-								</form>
-                            </div>
-                        </div>
+                        <a class="navbar-brand" href="<?=BASE_URL;?>"><img src="<?=$this->asset('/images/logo.png')?>" alt="logo"></a>
                     </div>
                 </div>
             </div>
@@ -41,11 +22,11 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?=BASE_URL;?>"><img src="<?=$this->asset('/images/logo.png')?>" alt="logo"></a>
+                    
 
                 </div>
 
-                <div class="collapse navbar-collapse navbar-right">
+                <div class="collapse navbar-collapse">
                     <?php
                         echo $this->menu()->getFrontMenu(WEB_LANG, 'class="nav navbar-nav"', 'class="dropdown"', 'class="dropdown-menu"');
                     ?>
